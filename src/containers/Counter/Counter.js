a7e42af7f6cee597665a84b811d1204d2bfb6c77
RavinduSathsara <rsathsara330@gmail.com>
2023-08-10 08:26:15 +0530

    add functional compenents to couter component

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,61 +1,50 @@
-import React, { Component } from "react";
-
+import React, { useState } from "react";
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
-class Counter extends Component {
-  state = {
-    counter: 0,
-  };
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
 
-  counterChangedHandler = (action, value) => {
+  const counterChangedHandler = (action, value) => {
     switch (action) {
       case "inc":
-        this.setState((prevState) => {
-          return { counter: prevState.counter + 1 };
-        });
+        setCounter((prevCounter) => prevCounter + 1);
         break;
       case "dec":
-        this.setState((prevState) => {
-          return { counter: prevState.counter - 1 };
-        });
+        setCounter((prevCounter) => prevCounter - 1);
         break;
       case "add":
-        this.setState((prevState) => {
-          return { counter: prevState.counter + value };
-        });
+        setCounter((prevCounter) => prevCounter + value);
         break;
       case "sub":
-        this.setState((prevState) => {
-          return { counter: prevState.counter - value };
-        });
+        setCounter((prevCounter) => prevCounter - value);
+        break;
+      default:
         break;
     }
   };
 
-  render() {
-    return (
-      <div>
-        <CounterOutput value={this.state.counter} />
-        <CounterControl
-          label="Increment"
-          clicked={() => this.counterChangedHandler("inc")}
-        />
-        <CounterControl
-          label="Decrement"
-          clicked={() => this.counterChangedHandler("dec")}
-        />
-        <CounterControl
-          label="Add 5"
-          clicked={() => this.counterChangedHandler("add", 5)}
-        />
-        <CounterControl
-          label="Subtract 5"
-          clicked={() => this.counterChangedHandler("sub", 5)}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <CounterOutput value={counter} />
+      <CounterControl
+        label="Increment"
+        clicked={() => counterChangedHandler("inc")}
+      />
+      <CounterControl
+        label="Decrement"
+        clicked={() => counterChangedHandler("dec")}
+      />
+      <CounterControl
+        label="Add 5"
+        clicked={() => counterChangedHandler("add", 5)}
+      />
+      <CounterControl
+        label="Subtract 5"
+        clicked={() => counterChangedHandler("sub", 5)}
+      />
+    </div>
+  );
+};
 
 export default Counter;
