c76acf7dc599c00b40594a9f5c006edc102ba62e
RavinduSathsara <rsathsara330@gmail.com>
2023-08-10 08:35:58 +0530

    change call to functional components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
-
+import React from "react";
 import Counter from "./containers/Counter/Counter";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Counter />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+};
 
 export default App;
